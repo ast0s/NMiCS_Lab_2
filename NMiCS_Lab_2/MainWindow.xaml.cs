868b1b61a74d5c0fa09402fcefd3ef0740ddcc02
BulatNick <bulatnikov.mykyta@vu.cdu.edu.ua>
2022-04-20 04:08:46 +0300

    Добавьте файлы проекта.

---
+++ NMiCS_Lab_2/MainWindow.xaml.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Windows;
+
+namespace NMiCS_Lab_2
+{
+    public partial class MainWindow : Window
+    {
+        public List<string> slar = new List<string>();
+
+        public double epsilon;
+        public double[,] matrixA;
+        public double[] matrixB;
+        public double[,] mainMatrix;
+
+        public double[,] transposedMatrixA;
+        public double[,] normalizedMatrixA;
+        public double[] normalizedMatrixB;
+        public double[,] normalizedMainMatrix;
+
+        public List<double> argumentsX = new List<double>();
+
+        public MainWindow()
+        {
+            Thread.CurrentThread.CurrentCulture = new System.Globalization.CultureInfo("en-US");
+            InitializeComponent();
+        }
+
+        private void OK_Button_Click(object sender, RoutedEventArgs e)
+        {
+            EquationField.Text = EquationField.Text.Replace("- ", "-").Replace("+", "").Replace("=", "");
+
+            if (EquationField.Text != "")
+                slar.Add(EquationField.Text);
+
+            EquationField.Text = "";
+        }
+        private void EpsilonOKButton_Click(object sender, RoutedEventArgs e)
+        {
+            epsilon = double.Parse(EpsilonField.Text);
+            EpsilonField.Text = "";
+        }
+        private void Reset_Button_Click(object sender, RoutedEventArgs e)
+        {
+            slar.Clear();
+            EquationField.Text = "";
+        }
+        private void ClearAll_Button_Click(object sender, RoutedEventArgs e)
+        {
+            slar.Clear();
+            argumentsX.Clear();
+
+            matrixA = null;
+            matrixB = null;
+            mainMatrix = null;
+
+            transposedMatrixA = null;
+            normalizedMatrixA = null;
+            normalizedMatrixB = null;
+            normalizedMainMatrix = null;
+
+            ResultField.Text = "";
+            EpsilonField.Text = "";
+            EquationField.Text = "";
+        }
+        private void Solve_Button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                GetArgumentsX();
+                GetMainMatrix();
+                GetMatrixA();
+                GetMatrixB();
+
+                GetTransposedMatrixA();
+                GetNormalizedMatrixA();
+                GetNormalizedMatrixB();
+
+                ZeidelMethod rm = new ZeidelMethod(normalizedMatrixA, normalizedMatrixB, argumentsX, epsilon);
+                argumentsX = rm.DoZeidelMethod();
+
+                if (argumentsX == null)
+                    ResultField.Text = "Something went wrong!";
+                else
+                    for (int i = 0; i < argumentsX.Count; i++)
+                        ResultField.Text += $"x{i + 1} = " + argumentsX[i] + "\n";
+
+            }
+            catch (Exception exception)
+            {
+                _ = MessageBox.Show($"An error has occured! Message: {exception.Message}");
+            }
+        }
+
+        public void GetArgumentsX()
+        {
+            int Xquantity = slar[0].Split(' ').Select(x => double.Parse(x)).ToArray().Length - 1;
+
+            for (int i = 0; i < Xquantity; i++)
+                argumentsX.Add(0);
+        }
+        public void GetMainMatrix()
+        {
+            double[][] temp = new double[argumentsX.Count][];
+
+            for (int i = 0; i < argumentsX.Count; i++)
+            {
+                temp[i] = new double[argumentsX.Count + 1];
+                temp[i] = slar[i].Split(' ').Select(x => double.Parse(x)).ToArray();
+            }
+
+            mainMatrix = new double[temp.GetLength(0), argumentsX.Count + 1];
+
+            for (int i = 0; i < mainMatrix.GetLength(0); i++)
+                for (int j = 0; j < mainMatrix.GetLength(1); j++)
+                {
+                    if(j != mainMatrix.GetLength(1) - 1)
+                        mainMatrix[i, j] = temp[i][j];
+                    else
+                        mainMatrix[i, j] = temp[i][j] * -1;
+                }
+        }
+        public void GetMatrixA()
+        {
+            matrixA = new double[mainMatrix.GetLength(0), mainMatrix.GetLength(1) - 1];
+
+            for (int i = 0; i < matrixA.GetLength(0); i++)
+                for (int j = 0; j < matrixA.GetLength(1); j++)
+                    matrixA[i, j] = mainMatrix[i, j];
+        }
+        public void GetMatrixB()
+        {
+            matrixB = new double[slar.Count];
+
+            for (int i = 0; i < matrixB.Length; i++)
+                matrixB[i] = mainMatrix[i, mainMatrix.GetLength(1) - 1] * -1;
+        }
+
+        public void GetTransposedMatrixA()
+        {
+            transposedMatrixA = new double[matrixA.GetLength(1), matrixA.GetLength(0)];
+
+            for (int i = 0; i < transposedMatrixA.GetLength(0); i++)
+                for (int j = 0; j < transposedMatrixA.GetLength(1); j++)
+                    transposedMatrixA[i, j] = matrixA[j, i];
+        }
+        public void GetNormalizedMatrixA()
+        {
+            normalizedMatrixA = new double[matrixA.GetLength(0), matrixA.GetLength(1)];
+
+            for (int i = 0; i < normalizedMatrixA.GetLength(0); i++)
+                for (int j = 0; j < normalizedMatrixA.GetLength(1); j++)
+                    for (int k = 0; k < normalizedMatrixA.GetLength(1); k++)
+                        normalizedMatrixA[i, j] += transposedMatrixA[i, k] * matrixA[k, j];
+        }
+        public void GetNormalizedMatrixB()
+        {
+            normalizedMatrixB = new double[matrixB.Length];
+
+            for (int i = 0; i < normalizedMatrixB.Length; i++)
+                for (int j = 0; j < argumentsX.Count; j++)
+                    normalizedMatrixB[i] += transposedMatrixA[i, j] * matrixB[j];
+        }
+    }
+}
---
+++ NMiCS_Lab_2/ZeidelMethod.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NMiCS_Lab_2
+{
+    public class ZeidelMethod
+    {
+        double[,] matrixA;
+        double[] matrixB;
+        List<double> currentArguments;
+        List<double> previousArguments = new List<double>();
+        double epsilon;
+
+        public ZeidelMethod(double[,] normalizedMatrixA, double[] normalizedMatrixB, List<double> argumentsX, double epsilon)
+        {
+            matrixA = normalizedMatrixA;
+            matrixB = normalizedMatrixB;
+            currentArguments = argumentsX;
+            this.epsilon = epsilon;
+
+            previousArguments.AddRange(currentArguments);
+        }
+
+        public List<double> DoZeidelMethod()
+        {
+            for (int i = 0; i < currentArguments.Count; i++)
+                FindX(i);
+
+            for (int i = 0; i < currentArguments.Count; i++)
+            {
+                if (Math.Abs(previousArguments[i] - currentArguments[i]) > epsilon)
+                {
+                    previousArguments.Clear();
+                    previousArguments.AddRange(currentArguments);
+                    return DoZeidelMethod();
+                }
+            }
+
+            return currentArguments;
+        }
+        public void FindX(int iteration)
+        {
+            double temp = 0;
+            for (int i = 0; i < currentArguments.Count; i++)
+                if (i != iteration)
+                    temp += matrixA[iteration, i] * currentArguments[i];
+
+            currentArguments[iteration] = (matrixB[iteration] - temp) / matrixA[iteration, iteration];
+        }
+    }
+}
